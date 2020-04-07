d4aee680c0adb103a40b2f8c5db5ac37219e41f4
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2020-04-07 03:09:43 +0200

    Some rework and add new file for checking performance of trained model

--- Breathing_1/end_to_end/temp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a= np.array([[1,2,3], [4,5,6]])
-b= np.array([[10,20,30], [40, 50, 60]])
-timesteps=np.array([[0.2, 0.3 ,0.4], [0.3, 0.4, 0.5]])
-
-
-
-print(a[timesteps==0.4])
